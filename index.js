0f4e2516288ac6c6e4453e475629f0cf465812b5
Shaahid Adams <shaahid.adams@gmail.com>
2024-10-19 00:44:27 +0200

    Basic scaffold for API in place

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { id } = require("tigerbeetle-node");
+const { createClient, CreateAccountError, CreateTransferError } = require("tigerbeetle-node");
+
+// Create a client object that we use as a sort of middleman object to do all the cool shit
+const client = createClient({
+    cluster_id: 0n,
+    replica_addresses: [process.env.TB_ADDRESS || '3000'],
+})
 const app = express();
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
-
+console.log("Initialisation completed successfully");
 // GET endpoint
 app.get('/', (req, res) => {
     res.send('Welcome to my API!');
